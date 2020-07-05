f1de487cd26cdb262e6798f8f83bc987d523e7c8
Tylermutai <paris@atlassan.com>
2020-07-05 15:02:45 +0300

    fixed dependencies

---
+++ src/main/java/com/testing/demo/controllers/ClientServerController.java
@@ -0,0 +1,89 @@
+package com.testing.demo.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/api", produces = "application/json;charset=UTF-8")
+//All our api request URLs will start with /api and will return Json
+public class ClientServerController {
+    final String SERVERS_FILE = "servers";
+
+    //clients registering as servers.
+    @PostMapping(value = "/server/register")
+    public Map<String, String> registerClientAsServer(HttpServletRequest request) throws IOException {
+        //Get our servers file. TODO: These operations will definitely be handled by a database.
+        String clientIP = request.getRemoteAddr();
+        String randomString = getAlphaNumericString(3);
+        String serverName = "Server_" + randomString;
+        System.out.println(clientIP);
+        System.out.println(serverName);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(SERVERS_FILE, true));
+        writer.write(serverName + "," + clientIP);
+        writer.close();
+        return Collections.singletonMap("message", "Successfully set up. Your server name is: " + serverName +
+                " with an IP address of: " + clientIP);
+    }
+
+
+    //clients registering as clients
+    @GetMapping(value = "/server/view")
+    public ResponseEntity<Map<String, ArrayList<String>>> getClientsRegisteredAsServers() throws IOException {
+        //JsonObjectBuilder jsonResponse = Json.createObjectBuilder();
+        File file = new File(SERVERS_FILE);
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+          /*  jsonResponse.add("message", "No Clients to connect to are available. You can be one by" +
+                    " clicking on the be a server button");
+            return new ResponseEntity<>(jsonResponse.build().toString(), HttpStatus.NOT_FOUND);*/
+            ArrayList<String> response = new ArrayList<>();
+            response.add("No Clients to connect to are available. You can be one by" +
+                    " clicking on the be a server button");
+            return new ResponseEntity<>(Collections.singletonMap("response", response), HttpStatus.NOT_FOUND);
+        }
+        String st;
+        ArrayList<String> builder = new ArrayList<>();
+        while ((st = br.readLine()) != null)
+            builder.add(st);
+        return ResponseEntity.ok(Collections.singletonMap("servers", builder));
+    }
+
+    // function to generate a random string of length n
+    private String getAlphaNumericString(int n) {
+
+        // chose a Character random from this String
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789"
+                + "abcdefghijklmnopqrstuvxyz";
+
+        // create StringBuffer size of AlphaNumericString
+        StringBuilder sb = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++) {
+
+            // generate a random number between
+            // 0 to AlphaNumericString variable length
+            int index
+                    = (int) (AlphaNumericString.length()
+                    * Math.random());
+
+            // add Character one by one in end of sb
+            sb.append(AlphaNumericString
+                    .charAt(index));
+        }
+
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/testing/demo/controllers/CustomErrorController.java
@@ -0,0 +1,54 @@
+package com.testing.demo.controllers;
+
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class CustomErrorController implements ErrorController {
+
+
+    @RequestMapping(value = "error", produces = "application/json;charset=UTF-8")
+    public Map<String, String> renderErrorPage(HttpServletRequest request) {
+        String errorMsg = "";
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        int httpErrorCode = 404;
+        if (status != null) {
+            httpErrorCode = Integer.parseInt(status.toString());
+        }
+        switch (httpErrorCode) {
+            case 400: {
+                errorMsg = "Http Error Code: 400. Bad Request";
+                break;
+            }
+            case 401: {
+                errorMsg = "Http Error Code: 401. Unauthorized";
+                break;
+            }
+            case 404: {
+                errorMsg = "Http Error Code: 404. Resource not found";
+                break;
+            }
+            case 500: {
+                errorMsg = "Http Error Code: 500. Internal Server Error";
+                break;
+            }
+            case 405: {
+                errorMsg = "Http Error Code: 405. Method not allowed";
+                break;
+            }
+        }
+        return Collections.singletonMap("error", errorMsg);
+    }
+
+    @Override
+    public String getErrorPath() {
+        System.out.println("############################");
+        return "/error";
+    }
+}
