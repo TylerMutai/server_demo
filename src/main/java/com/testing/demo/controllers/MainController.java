6ccb46d17399a7f8c715eb5c42208bd454e980d4
Tylermutai <paris@atlassan.com>
2020-07-05 19:44:18 +0300

    first commit

--- src/main/java/com/testing/demo/controllers/MainController.java
+++ src/main/java/com/testing/demo/controllers/MainController.java
@@ -1,5 +1,6 @@
 package com.testing.demo.controllers;
 
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -15,7 +16,7 @@ public class MainController extends TextWebSocketHandler {
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         // Thread.sleep(3000); // simulated delay
-       /* if (sessions.size() > 1) {
+        /*if (sessions.size() > 1) {
             //The second client has connected. Send the respective IP addresses.
             WebSocketSession theOtherClient = null;
             for (WebSocketSession webSocketSession : sessions) {
@@ -33,14 +34,20 @@ public class MainController extends TextWebSocketHandler {
             }
             TextMessage msg = new TextMessage("IP: " + remoteIpOfOtherClient);
             session.sendMessage(msg);
-        }*/
+        }
         session.sendMessage(new TextMessage(sessions.size() + ""));
         //session.getRemoteAddress();
-        //session.sendMessage(msg);
+        //session.sendMessage(msg);*/
     }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+        sessions.remove(session);
     }
 }
